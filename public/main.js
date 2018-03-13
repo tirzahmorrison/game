d08abb39b0d4ae9049b7ad0a8be5de7292d05724
Tirzah Morrison <tirzahmorrison@icloud.com>
2018-03-13 15:52:33 -0400

    new file

--- public/main.js
+++ public/main.js
@@ -1,5 +1,48 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+const user;
+const choose = function(choice) {
+    user = choice;
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const computerChoice = Math.random();
+if (computerChoice < 0.34) {
+    computerChoice = "rock";
+}
+else if (computerChoice < 0.67) {
+    computerChoice = "paper";
+}   
+else {
+    computerChoice = "scissors";
+}
+
+const compare = function(choice1, choice2) {
+    if (choice1 === choice2) {
+        document.getElementById("result").innerHTML = "Tie!";
+    }
+    else if (choice1 === "rock") {
+        if (choice2 === "scissors") {
+            document.getElementById("result").innerHTML = "You Win!";
+        }
+        else {
+            document.getElementById("result").innerHTML = "You Lose!";
+        }
+    }
+    else if (choice1 === "paper") {
+        if (choice2 === "rock") {
+            document.getElementById("result").innerHTML = "You Win!";
+        }
+        else if (choice2 ==="scissors") {
+            document.getElementById("result").innerHTML = "You Lose!";
+        }
+    }
+    else if (choice1 === "scissors") {
+        if (choice2 === "rock") {
+            document.getElementById("result").innerHTML = "You Lose!";
+        }
+        else if (choice2 === "paper") {
+            document.getElementById("result").innerHTML = "You Win!";
+        }
+    }
+    else {
+        document.getElementById("result").innerHTML = "ERROR!";
+    }
+}
