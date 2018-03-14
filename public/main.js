db0beddab7f400d2c7f50d219c711550c904a2c7
Tirzah Morrison <tirzahmorrison@icloud.com>
2018-03-13 21:50:38 -0400

    elegant

--- public/main.js
+++ public/main.js
@@ -3,12 +3,12 @@ const game = {
   choices: ["rock", "paper", "scissors"],
   players: [
     {
-      choice: "",
-      name: ""
+      choice: ""
+      
     },
     {
-      choice: "",
-      name: ""
+      choice: ""
+      
     }
   ]
 }
@@ -17,16 +17,16 @@ const gameStart = () => {
 
 }
 //As a user I should be able to see three buttons for each player
+
+
 //as a user, I should be able to pick an option for each player
- //As a user, after each player makes their selection, I should be told who won and the game should be over
-
-
-//As a user I should be able to pick my name
-
-const playerNameChange = (index,newName) => {
-
+const playerChoice = (index, choice) => {
+  game.players[index].choice= choice
 }
 
+//As a user, after each player makes their selection, I should be told who won and the game should be over
+
+const choseWinner 
 
 
 
