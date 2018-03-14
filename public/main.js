5a8445b478d1c0debeabc1e92a8f7cd4e7ff488d
Tirzah Morrison <tirzahmorrison@icloud.com>
2018-03-13 21:35:40 -0400

    user stories

--- public/main.js
+++ public/main.js
@@ -1,16 +1,59 @@
 const game = {
-  winner:"",
-  choices:["rock", "paper", "scissors"],
-  players:[
+  winner: "",
+  choices: ["rock", "paper", "scissors"],
+  players: [
     {
-      choice:"",
-      name:""
+      choice: "",
+      name: ""
     },
     {
-      choice:"",
-      name:""
+      choice: "",
+      name: ""
     }
   ]
 }
 
-const gameStart=()=>
+const gameStart = () => {
+
+}
+//As a user I should be able to see three buttons for each player
+//as a user, I should be able to pick an option for each player
+ //As a user, after each player makes their selection, I should be told who won and the game should be over
+
+
+//As a user I should be able to pick my name
+
+const playerNameChange = (index,newName) => {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', gameStart)
+
