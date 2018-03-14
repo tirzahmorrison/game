f43f38c540c0e6e905cc7d37732e283f21387e34
Tirzah Morrison <tirzahmorrison@icloud.com>
2018-03-13 22:30:17 -0400

    try

--- public/main.js
+++ public/main.js
@@ -12,11 +12,30 @@ const game = {
     }
   ]
 }
-
-const gameStart = () => {
-
-}
 //As a user I should be able to see three buttons for each player
+const gameStart = () => {
+ document.querySelector(".player1 .rock").addEventListener("click",()=>{
+   playerChoice(0,"rock")
+ })
+ document.querySelector(".player1 .paper").addEventListener("click",()=>{
+    playerChoice(0,"paper")
+  })
+  document.querySelector(".player1 .scissors").addEventListener("click",()=>{
+    playerChoice(0,"scissors")
+  })
+  document.querySelector(".player2 .rock").addEventListener("click",()=>{
+    playerChoice(1,"rock")
+  })
+  document.querySelector(".player2 .paper").addEventListener("click",()=>{
+    playerChoice(1,"paper")
+  })
+  document.querySelector(".player2 .scissors").addEventListener("click",()=>{
+    playerChoice(1,"scissors")
+  })
+
+  document.querySelector(".go").addEventListener("click", choseWinner)
+}
+
 
 
 //as a user, I should be able to pick an option for each player
